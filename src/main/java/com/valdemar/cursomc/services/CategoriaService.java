ffebbc9d9326ce704943596d90f5c0e521a661dd
Valdemar1980 <valdemar.java@gmail.com>
2022-03-14 21:46:39 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/valdemar/cursomc/services/CategoriaService.java
+++ src/main/java/com/valdemar/cursomc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.valdemar.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.valdemar.cursomc.domain.Categoria;
 import com.valdemar.cursomc.repositories.CategoriaRepository;
+import com.valdemar.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +18,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}	
 
 }
