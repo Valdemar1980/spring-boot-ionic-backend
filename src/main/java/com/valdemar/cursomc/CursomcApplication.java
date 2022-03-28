2e896793ed8318d63b6f09132eb6bb312a76eab0
Valdemar1980 <valdemar.java@gmail.com>
2022-03-28 19:21:17 -0300

    Pedido,EstadoPagamento e Pagamento

--- src/main/java/com/valdemar/cursomc/CursomcApplication.java
+++ src/main/java/com/valdemar/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.valdemar.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,36 +13,50 @@ import com.valdemar.cursomc.domain.Cidade;
 import com.valdemar.cursomc.domain.Cliente;
 import com.valdemar.cursomc.domain.Endereco;
 import com.valdemar.cursomc.domain.Estado;
+import com.valdemar.cursomc.domain.Pagamento;
+import com.valdemar.cursomc.domain.PagamentoComBoleto;
+import com.valdemar.cursomc.domain.PagamentoComCartao;
+import com.valdemar.cursomc.domain.Pedido;
 import com.valdemar.cursomc.domain.Produto;
+import com.valdemar.cursomc.domain.enums.EstadoPagamento;
 import com.valdemar.cursomc.domain.enums.TipoCliente;
 import com.valdemar.cursomc.repositories.CategoriaRepository;
 import com.valdemar.cursomc.repositories.CidadeRepository;
 import com.valdemar.cursomc.repositories.ClienteRepository;
 import com.valdemar.cursomc.repositories.EnderecoRepository;
 import com.valdemar.cursomc.repositories.EstadoRepository;
+import com.valdemar.cursomc.repositories.PagamentoRepository;
+import com.valdemar.cursomc.repositories.PedidoRepository;
 import com.valdemar.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursomcApplication implements  CommandLineRunner {
+public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -88,6 +103,21 @@ public class CursomcApplication implements  CommandLineRunner {
 	
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
 		
 	}
 
